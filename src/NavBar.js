295ec316230050a74376b8b9f03f0ab559866500
Caleb Kavanaugh <calebck@hotmail.com>
2020-03-01 22:12:42 -0500

    Done.

---
+++ src/NavBar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white"
+};
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+      <NavLink
+        to="/"
+        exact
+        style={link}
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={link}
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={link}
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        About
+      </NavLink>
+    </div>
+    )
+  }
+}
+
+export default NavBar
