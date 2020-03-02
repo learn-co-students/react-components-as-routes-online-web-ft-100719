3a51ed4916691ba52b7a0d486d50b4d8fe302784
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-02 10:46:16 -0500

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Messages extends Component {
+    render() {
+        return (
+            <div>
+                <h1>These are your messages!</h1>
+            </div>
+        )
+    }
+}
+
+export default Messages
